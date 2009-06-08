ce67bbb55347705377d822d31930915f9e3b623d
jturk <jturk@3acbd82f-b2a5-4b0e-9dcd-940c2165acb0>
2009-06-08 17:43:30 +0000

    installable
    git-svn-id: https://source.sunlightlabs.com/pymods/django-feedinator@110 3acbd82f-b2a5-4b0e-9dcd-940c2165acb0

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+long_description = ''#open('README.rst').read()
+
+setup(
+    name='django-feedinator',
+    version="0.1",
+    package_dir={'feedinator': 'feedinator'},
+    packages=['feedinator', 'feedinator.templatetags'],
+    package_data={'feedinator': ['templates/feedinator/*.html']},
+    description='Django feedinator',
+    author='Jeremy Carbaugh',
+    author_email='jcarbaugh@sunlightfoundation.com',
+    license='BSD License',
+    url='http://github.com/sunlightlabs/django-feedinator/',
+    long_description=long_description,
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+    ],
+)

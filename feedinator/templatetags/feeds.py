d2a929bfe217134273c902fbe81bd22774646b27
Jeremy Carbaugh <jcarbaugh@gmail.com>
2009-12-10 15:57:15 -0500

    add offset argument to headlines template tag

--- feedinator/templatetags/feeds.py
+++ feedinator/templatetags/feeds.py
@@ -5,6 +5,8 @@ from feedinator.models import FeedEntry
 register = template.Library()
 
 @register.simple_tag
-def headlines(codenames, count):
-    entries = FeedEntry.objects.filter(feed__codename__in=codenames.split(',')).order_by("-date_published")[:int(count)]
+def headlines(codenames, count, offset=0):
+    offset = int(offset)
+    index = int(count) + offset
+    entries = FeedEntry.objects.filter(feed__codename__in=codenames.split(',')).order_by("-date_published")[offset:index]
     return render_to_string("feedinator/headlines.html", {"entries": entries})

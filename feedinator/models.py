89b0500907c1d915e261b46d7fca16621d41ce33
Jeremy Carbaugh <jcarbaugh@gmail.com>
2012-08-30 14:23:09 -0400

    changes for django 1.4's new timezone features

--- feedinator/models.py
+++ feedinator/models.py
@@ -1,7 +1,9 @@
+import datetime
+
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
-import datetime
+from pytz import common_timezones
 
 FEED_TYPE_CHOICES = (
     (u'rss', u'RSS'),
@@ -15,18 +17,22 @@ FEED_TYPE_CHOICES = (
     (u'atom-1.0', u'Atom 1.0'),
 )
 
+TIMEZONE_CHOICES = [(tz, tz) for tz in common_timezones]
+
+
 class Feed(models.Model):
     url = models.URLField(verify_exists=False)
     codename = models.CharField(max_length=128, blank=True)
     type = models.CharField(max_length=16, choices=FEED_TYPE_CHOICES, blank=True, null=True)
     title = models.CharField(max_length=225)
     link = models.URLField(verify_exists=False, blank=True)
+    timezone = models.CharField(max_length=32, choices=TIMEZONE_CHOICES, default='UTC')
     description = models.TextField(blank=True)
     ttl = models.IntegerField(default=60)
     date_added = models.DateTimeField(auto_now_add=True)
     last_fetched = models.DateTimeField(blank=True, null=True)
     next_fetch = models.DateTimeField()
-    
+
     def __unicode__(self):
         return self.title
 
@@ -37,7 +43,8 @@ class Feed(models.Model):
         else:
             self.next_fetch = self.last_fetched + datetime.timedelta(0, 0, 0, 0, self.ttl)
         super(Feed, self).save(*args, **kwargs)
-        
+
+
 class Subscription(models.Model):
     feed = models.ForeignKey(Feed, related_name='subscriptions')
     content_type = models.ForeignKey(ContentType)
@@ -47,9 +54,11 @@ class Subscription(models.Model):
     def __unicode__(self):
         return '%s to %s' % (self.subscriber, self.feed)
 
+
 class FeedEntryManager(models.Manager):
     pass
-    
+
+
 class FeedEntry(models.Model):
     objects = FeedEntryManager()
     uid = models.CharField(max_length=255)
@@ -64,19 +73,20 @@ class FeedEntry(models.Model):
     date_published = models.DateTimeField(blank=True, null=True)
     date_updated = models.DateTimeField(blank=True, null=True)
     last_fetched = models.DateTimeField()
-    
+
     class Meta:
         ordering = ['-date_published']
-        
+
     def __unicode__(self):
         return u"%s: %s" % (self.feed.title, self.title)
-        
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=128)
     feed_entry = models.ForeignKey(FeedEntry, related_name="tags")
-    
+
     class Meta:
         ordering = ['name']
-    
+
     def __unicode__(self):
         return self.name
